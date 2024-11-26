5a98e23db8ffd914fafd40818671b21b34dfbd6b
fernando <fernando.morais0703@gmail.com>
2024-11-26 09:26:14 -0300

    Add pgadmin4 service to docker compose

--- startup.sh
+++ startup.sh
@@ -17,12 +17,18 @@ POSTGRES_USER_DEFAULT=dvdrental
 POSTGRES_PASSWORD_DEFAULT=dvdrental
 URL_DEFAULT=https://neon.tech/postgresqltutorial/dvdrental.zip
 HOST_PORT_DEFAULT=5432
+PGADMIN_DEFAULT_EMAIL_DEFAULT="dvdrental@domain.com"
+PGADMIN_DEFAULT_PASSWORD_DEFAULT="dvdrental"
+PGADMIN_DEFAULT_LISTEN_PORT_DEFAULT=8080
 
 POSTGRES_DB=${POSTGRES_DB_DEFAULT}
 POSTGRES_USER=${POSTGRES_USER_DEFAULT}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD_DEFAULT}
 URL=${URL_DEFAULT}
 HOST_PORT=${HOST_PORT_DEFAULT}
+PGADMIN_DEFAULT_EMAIL=${PGADMIN_DEFAULT_EMAIL_DEFAULT}
+PGADMIN_DEFAULT_PASSWORD=${PGADMIN_DEFAULT_PASSWORD_DEFAULT}
+PGADMIN_DEFAULT_LISTEN_PORT=${PGADMIN_DEFAULT_LISTEN_PORT_DEFAULT}
 
 if [ "$option" == "2" ]; then
     echo "Manual Configuration ..."
@@ -41,11 +47,24 @@ if [ "$option" == "2" ]; then
     echo -n "Set host port to run the service [5432]: "
     read HOST_PORT
     HOST_PORT="${HOST_PORT:-${HOST_PORT_DEFAULT}}"
+
+    echo -n "Set pgadmin default email [dvdrental@domain.com]: "
+    read PGADMIN_DEFAULT_EMAIL
+    PGADMIN_DEFAULT_EMAIL="${PGADMIN_DEFAULT_EMAIL:-${PGADMIN_DEFAULT_EMAIL_DEFAULT}}"
+
+    echo "Set pgadmin default password [dvdrental]: "
+    read -s PGADMIN_DEFAULT_PASSWORD
+    PGADMIN_DEFAULT_PASSWORD="${PGADMIN_DEFAULT_PASSWORD:-${PGADMIN_DEFAULT_PASSWORD_DEFAULT}}"
+
+    echo -n "Set pgadmin listen port [8080]: "
+    read PGADMIN_DEFAULT_LISTEN_PORT
+    PGADMIN_DEFAULT_LISTEN_PORT="${PGADMIN_DEFAULT_LISTEN_PORT:-${PGADMIN_DEFAULT_LISTEN_PORT_DEFAULT}}"
 elif [ "$option" == "3" ]; then
     echo "Skipping Configuration ..."
     
-    sudo docker system prune --filter label=CLEARBEFORESTART=YES -f
-    sudo docker compose up --build
+    sudo docker compose down
+    sudo docker system prune --filter label=CLEARBEFORESTART=TRUE -f
+    sudo docker compose up --build -d
 
     exit 0
 else
@@ -57,8 +76,12 @@ echo "POSTGRES_USER=${POSTGRES_USER}" >> .env
 echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> .env
 echo "URL=https://neon.tech/postgresqltutorial/dvdrental.zip" >> .env
 echo "HOST_PORT=${HOST_PORT}" >> .env
+echo "PGADMIN_DEFAULT_EMAIL=${PGADMIN_DEFAULT_EMAIL}" >> .env
+echo "PGADMIN_DEFAULT_PASSWORD=${PGADMIN_DEFAULT_PASSWORD}" >> .env
+echo "PGADMIN_DEFAULT_LISTEN_PORT=${PGADMIN_DEFAULT_LISTEN_PORT}" >> .env
 
-sudo docker system prune --filter label=CLEARBEFORESTART=YES -f
-sudo docker compose up --build
+sudo docker compose down
+sudo docker system prune --filter label=CLEARBEFORESTART=TRUE -f
+sudo docker compose up --build -d
 
 exit 0
